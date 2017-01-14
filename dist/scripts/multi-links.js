6c277d33fbd2387e13871de069e5953774973168
Noam <modainoa.m@gmail.com>
2017-01-14 21:02:37 +0200

    Remove Stat-Counter

--- dist/scripts/multi-links.js
+++ dist/scripts/multi-links.js
@@ -29,18 +29,6 @@
          }, 'xml');
      });
 
-// Start of StatCounter Code for Default Guide
-  var sc_project=11215833;
-  var sc_invisible=1;
-  var sc_security="77358879";
-  var scJsHost = (("https:" == document.location.protocol) ?
-  "https://secure." : "http://www.");
-  document.write("<sc"+"ript type='text/javascript' src='" +
-  scJsHost+
-  "statcounter.com/counter/counter.js'></"+"script>");
-
-// End of StatCounter Code for Default Guide
-
 
 var year = new Date().getFullYear();
 
